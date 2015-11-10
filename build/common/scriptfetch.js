70ed49e288a650c06977bd1bf198af3a2297bf58
phoenix <phoenix>
2015-11-10 20:54:21 +0800

    change into related path

--- build/common/scriptfetch.js
+++ build/common/scriptfetch.js
@@ -3,7 +3,7 @@
     var mydir, path, scripts;
     scripts = document.getElementsByTagName('script');
     path = scripts[scripts.length - 1].src.split('?')[0];
-    return mydir = path.split('/').slice(0, -1).join('/') + '/';
+    return mydir = path.split('/').slice(3, -1).join('/') + '/';
   };
 
 }).call(this);

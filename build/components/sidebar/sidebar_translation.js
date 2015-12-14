234245cbe9117eec56144ac98854ad9ee50fea6c
phoenix <phoenix>
2015-12-14 15:50:17 +0800

    sidebar is updated

--- build/components/sidebar/sidebar_translation.js
+++ build/components/sidebar/sidebar_translation.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('zh', {"Hello, {{model.user}}":"你好，{{model.user}}"});
+    gettextCatalog.setStrings('zh', {"Hello, {{model.user}}":"你好, {{model.user}}"});
 /* jshint +W100 */
 }]);

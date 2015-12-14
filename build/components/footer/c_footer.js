bb7193f85ed9d13c6c3d5467aa7b9206d586ceef
phoenix <phoenix>
2015-12-14 16:41:39 +0800

    loading template path has been changed

--- build/components/footer/c_footer.js
+++ build/components/footer/c_footer.js
@@ -135,5 +135,5 @@ Create an angular module called footer
 }).call(this);
 
 angular.module('OOD_Footer').run(['$templateCache', function ($templateCache) {
-	$templateCache.put('./lib/components/footer/footer.html', '<div class="footer"><span class="copyright"> <span>Copyright © {{model.copyright}}</span><span class="line">|</span> <spen>Version: {{model.version}}</spen></span><span class="help"><span ng-repeat-start="site in model.websites"><a ng-href="{{model.getLink(site)}}">&nbsp;{{model.getName(site)}}&nbsp;</a></span><span ng-repeat-end="ng-repeat-end" ng-show="{{$index}}&lt;{{model.lenOfSites-1}}" class="line">|</span></span></div>');
+	$templateCache.put('lib/components/footer/footer.html', '<div class="footer"><span class="copyright"> <span>Copyright © {{model.copyright}}</span><span class="line">|</span> <spen>Version: {{model.version}}</spen></span><span class="help"><span ng-repeat-start="site in model.websites"><a ng-href="{{model.getLink(site)}}">&nbsp;{{model.getName(site)}}&nbsp;</a></span><span ng-repeat-end="ng-repeat-end" ng-show="{{$index}}&lt;{{model.lenOfSites-1}}" class="line">|</span></span></div>');
 }]);
